1498249216cce3c4194ce90092dc2e72378921d4
AyakorK <guillaume.moret@yahoo.com>
2023-05-02 09:23:41 +0200

    Initial Commit

---
+++ Restaurant/src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module com.coding.restaurant.restaurant {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens com.coding.restaurant.restaurant to javafx.fxml;
+    exports com.coding.restaurant.restaurant;
+}
