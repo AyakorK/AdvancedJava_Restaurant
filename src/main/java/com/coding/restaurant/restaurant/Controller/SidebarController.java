059026e8912e2d22cf39bd98c636e51c9f011579
Théo Landemaine <91159174+TheoLandemaine@users.noreply.github.com>
2023-05-03 16:10:04 +0200

    Change design sidebar && create Home fxml

--- src/main/java/com/coding/restaurant/restaurant/Controller/SidebarController.java
+++ src/main/java/com/coding/restaurant/restaurant/Controller/SidebarController.java
@@ -3,6 +3,7 @@ package com.coding.restaurant.restaurant.Controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
@@ -23,14 +24,29 @@ public class SidebarController implements Initializable {
     @FXML
     private VBox vboxTest;
 
+    @FXML
+    private AnchorPane acpHome;
+
+    @FXML
+    private ImageView imgLogo;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+
+        acpCentre.getChildren().remove(vboxTest);
+
+        imgLogo.setOnMouseClicked(e -> {
+            acpCentre.getChildren().removeAll(vboxTest);
+            acpCentre.getChildren().add(acpHome);
+        });
+
         btnMenu.setOnMouseClicked(e -> {
-            acpCentre.getChildren().addAll(vboxTest);
+            acpCentre.getChildren().removeAll(acpHome);
+            acpCentre.getChildren().add(vboxTest);
         });
 
         btnCommand.setOnMouseClicked(e -> {
-            acpCentre.getChildren().remove(vboxTest);
+            acpCentre.getChildren().removeAll(vboxTest, acpHome);
         });
 
     }
