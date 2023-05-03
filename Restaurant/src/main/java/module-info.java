04ac503f5b3fb5132f348b7cb8cdc4eb463cba91
AyakorK <guillaume.moret@yahoo.com>
2023-05-03 09:35:27 +0200

    Fix tilesfx error

--- Restaurant/src/main/java/module-info.java
+++ Restaurant/src/main/java/module-info.java
@@ -6,7 +6,7 @@ module com.coding.restaurant.restaurant {
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
+    //requires eu.hansolo.tilesfx;
 
     opens com.coding.restaurant.restaurant to javafx.fxml;
     exports com.coding.restaurant.restaurant;
