2f8a684fb9d8ab5e2f3f0ff1c59b743760a8a9f8
AyakorK <guillaume.moret@yahoo.com>
2023-05-04 22:32:36 +0200

    Link the button to create a new meal to the front

--- src/main/java/com/coding/restaurant/restaurant/controllers/NewMealController.java
+++ src/main/java/com/coding/restaurant/restaurant/controllers/NewMealController.java
@@ -1,11 +1,15 @@
 package com.coding.restaurant.restaurant.controllers;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -32,6 +36,9 @@ public class NewMealController implements Initializable {
   @FXML
   private TextField txfType;
 
+  @FXML
+  private Button btnBack;
+
 
   public void addMeal(String name, String description, Double price, String image, Boolean isActive, String type) {
     DatabaseManager.addMeal(name, description, price, image, isActive, type);
@@ -48,7 +55,24 @@ public class NewMealController implements Initializable {
       String type = txfType.getText();
 
       addMeal(name, description, price, image, isActive, type);
+      goBack();
     });
+
+    btnBack.setOnAction(event -> goBack());
+  }
+
+  public void goBack() {
+    try {
+      // Load the worker view & set it as the current view
+      FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/coding/restaurant/restaurant/menu.fxml"));
+      Node workerView = loader.load();
+
+      AnchorPane acpInWorker = (AnchorPane) btnValidateMeal.getParent().getParent().getParent();
+
+      acpInWorker.getChildren().setAll(workerView);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
   }
 }
 
