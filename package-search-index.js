08049a1ce1030a4a31210329bd9ef7f97285879c
Baptiste Dumoulin <91154604+Ptitbapt@users.noreply.github.com>
2023-05-05 17:03:41 +0200

    JavaDoc generated

---
+++ package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.coding.restaurant.restaurant","l":"com.coding.restaurant.restaurant"},{"m":"com.coding.restaurant.restaurant","l":"com.coding.restaurant.restaurant.controllers"},{"m":"com.coding.restaurant.restaurant","l":"com.coding.restaurant.restaurant.models"}];updateSearchResults();
